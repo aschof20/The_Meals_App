cbaf91990a82df85a1e4e61dc8c8347db10915c9
Alice Schofield <a.v.schofield@gmail.com>
2021-11-14 14:39:25 +1000

    Initial Screen File Setup

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading';
+const fetchFonts = ()=>{
+  return(Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+
+  }))
+}
+
 
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  if(!fontLoaded){
+    return     <AppLoading
+    startAsync={fetchFonts}
+    onFinish={() => setFontLoaded(true)}
+    onError={(err) => console.log(err)}
+/>
+  }
   return (
     <View style={styles.container}>
-      <Text>Open uthest App.js to stahellog on your app!</Text>
+      <Text>Open up App.js to stahellog on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +34,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
