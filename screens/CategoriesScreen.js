655892ce18cd9f8ea96508bcb16dacd382c954ef
Alice Schofield <a.v.schofield@gmail.com>
2021-11-15 18:20:31 +1000

    Passing data between different screens on the NavigationStack

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,28 +1,54 @@
-import React from 'react'
-import {View, Text, FlatList, StyleSheet, Button} from 'react-native'
-import {CATEGORIES} from '../data/dummy-data'
+import React from 'react';
+import { View, Text, FlatList, StyleSheet, Button, TouchableOpacity, Platform} from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+import Colors from "../constants/Colors"
 
 
-const renderGridItem = (itemData)=>{
-    return <View style={styles.gridItem}><Text>{itemData.item.title}</Text></View>
+const CategoriesScreen = (props) => {
+	// Props has a whole lot of information/ objects attached to it
+	//console.log(props)
+    const renderGridItem = (itemData) => {
+        return (
+        <TouchableOpacity style={styles.gridItem} onPress={()=>{
+            props.navigation.navigate({routeName: 'CategoryMeals', params:{
+				categoryId: itemData.item.id
+			}})
+        }}>	
+            <View >
+                <Text>{itemData.item.title}</Text>
+            </View>
+        </TouchableOpacity>
+        );
+    };
+	return (
+		<FlatList
+			keyExtractor={(item, index) => item.id}
+			data={CATEGORIES}
+			renderItem={renderGridItem}
+			numColumns={2}
+		/>
+	);
+};
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
 }
-const CategoriesScreen = props => {
-// Props has a whole lot of information/ objects attached to it
-    //console.log(props)
 
-    return (
-       <FlatList keyExtractor={(item, index)=> item.id} data={CATEGORIES} renderItem={renderGridItem} numColumns={2}/>
-    )
-}
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	gridItem: {
+		flex: 1,
+		margin: 15,
+		height: 150,
+	},
+});
 
-const styles = StyleSheet.create({screen:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-}, gridItem:{
-    flex: 1,
-    margin: 15, height: 150
-}})
- 
-
-export default CategoriesScreen
+export default CategoriesScreen;
--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import {View, Text, StyleSheet, Button} from 'react-native'
+import { CATEGORIES } from '../data/dummy-data'
 
 const CategoryMealScreen = props => {
+    const catId = props.navigation.getParam('categoryId')
+    const selectedCategory = CATEGORIES.find(cat => cat.id)
     return (
         <View style={styles.screen}>
             <Text>The Category Meal Screen Screen</Text>
+            <Text>{selectedCategory.title}</Text>
             <Button title="Go To Details" onPress={()=> {
                 // routeName is from those setup in MealsNavigator.js
                 props.navigation.navigate({routeName: 'MealDetail'})
