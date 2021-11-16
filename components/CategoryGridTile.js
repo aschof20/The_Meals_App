783804c1f02ae75bea7dedb8a5ca9c4d6d5c89ad
Alice Schofield <a.v.schofield@gmail.com>
2021-11-16 16:07:15 +1000

    Rendering Meal details when click on category

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
     shadowColor: 'black',
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 10,
+    shadowRadius: 40,
     elevation: 3,
     padding: 15,
     justifyContent: 'flex-end',
