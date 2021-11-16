da00743f214968f0b30dcf2268dfe5109556ffd6
Alice Schofield <a.v.schofield@gmail.com>
2021-11-16 16:22:16 +1000

    Adding link to meal details screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,23 +1,40 @@
 import React from 'react'
-import {View, Text, StyleSheet, Button} from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import { MEALS } from '../data/dummy-data'
 
-const MealDetailScreen = props => {
-    return (
-        <View style={styles.screen}>
-            <Text>The Meal Detail Screen Screen</Text>
-            <Button title="Go Back to Categories" onPress={()=>{
-                // Return to the top most screen in the stack - root screen
-                props.navigation.popToTop()
-            }}/>
-        </View>
-    )
+const MealDetailScreen = (props) => {
+  const mealId = props.navigation.getParam('mealId')
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId)
+  return (
+    <View style={styles.screen}>
+      <Text>{selectedMeal.title}</Text>
+      <Button
+        title="Go Back to Categories"
+        onPress={() => {
+          // Return to the top most screen in the stack - root screen
+          props.navigation.popToTop()
+        }}
+      />
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({screen:{
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam('mealId')
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId)
+
+  return {
+    headerTitle: selectedMeal.title,
+  }
+}
+
+const styles = StyleSheet.create({
+  screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-}})
- 
+    alignItems: 'center',
+  },
+})
 
 export default MealDetailScreen
